918728d6692f47ca76dbd5cc3d4c1a4b68696a4d
GordonWoodhouse <22019454@woodhouse.ac.uk>
2023-06-28 15:42:29 +0100

    Goodbye vs hello

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Message(){
     const name = 'Gordon'
     if (name)
-        return <h1>Hello {name}</h1>;
-    return <h1>Hello World</h1>;
+        return <h1>GoodBye{name}</h1>;
+    return <h1>Goodbye World</h1>;
 };
 
 export default Message;
